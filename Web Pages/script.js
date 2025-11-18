c64dec2877d18638ff73c26d92a5288dc4c2feda
Lee.edu <ST10485444@rcconnect.edu.za>
2025-11-18 18:38:09 +0200

    Added JavaScript functionality to enhance user interaction on the homepage and services pages. Updated CSS for better responsiveness and modified HTML structure for improved accessibility.

---
+++ Web Pages/script.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  //  Autoplay 
+   autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    loop: true,
+  },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+        768: {
+        slidesPerView: 2,
+      },
+        1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
